35ce4a0c3c71bb4deae1868458d962edddc21184
paaatcha <pacheco.comp@gmail.com>
2017-10-18 17:36:26 -0200

    Updating assing8 and fix 7

--- assingment-8/assing8.py
+++ assingment-8/assing8.py
@@ -26,12 +26,7 @@ def checkingDataset (path):
     else:
         print 'The database has already been downloaded'
         
-# Euclidean distance
-def distance (x,y):
-    x = np.asarray(x)
-    y = np.asarray(y)
-    return np.sqrt(np.power(x-y,2).sum(axis=0))        
-        
+       
 def getIris (path):
     irisIn = np.loadtxt(path+'/iris.data',delimiter=',',usecols=(0,1,2,3))
     irisOut = list()
@@ -43,35 +38,25 @@ def getIris (path):
             except IndexError:
                 pass
             
+   
+    with open('iris_data.mtx','w') as f:
+        f.write('% Matrix Market\n')
+        f.write(str(irisIn.shape[0])+' '+str(irisIn.shape[1])+' '+str(irisIn.shape[0]*irisIn.shape[1])+'\n')
+        for i1 in range(irisIn.shape[0]):
+            for i2 in range(irisIn.shape[1]):
+                f.write(str(i1+1)+' '+str(i2+1)+' '+str(irisIn[i1,i2])+'\n')
+    
     with open('iris_class.txt','w') as f:
-        f.write('\n'.join(irisOut))           
+        for sample in irisOut:            
+            f.write(sample)    
+            f.write('\n')  
+
 
     return irisIn, irisOut
 
-def generateMtx (irisIn):
-    m,n = irisIn.shape
-    arch = open('iris.mtx','w')
-    arch.write (str(m)+' '+str(m)+' '+str(m*m)+'\n')
-    mtx = list()
-    for i1 in xrange(m):
-        for i2 in xrange(m):
-            #arch.write(str(i1+1)+' '+str(i2+1)+' '+str(distance(irisIn[i1,:], irisIn[i2,:]))+'\n')
-            mtx.append([i1+1, i2+1, distance(irisIn[i1,:], irisIn[i2,:])])
-            
-    # Normalizing
-    mtx = np.asarray(mtx)
-    mtxMax = mtx[:,2].max()    
-    mtx[:,2] = 1-(mtx[:,2]/mtxMax)
-    
-    for m in mtx:
-        arch.write(str(int(m[0]))+' '+str(int(m[1]))+' '+str(m[2])+'\n')
-    
-    arch.close()
-    
-    
 
-def generateIdsTrainTest (siz, perc='0.3'):
-    nTest = int(round(150*perc))
+def generateIdsTrainTest (siz, perc='0.3',irisOut=None):
+    nTest = int(round(150*0.3))
     allIds = range(siz)
     shuffle(allIds)
     
@@ -84,9 +69,24 @@ def generateIdsTrainTest (siz, perc='0.3'):
             
     with open('iris_train.txt','w') as f:
         for i in allIds[nTest:]:
-            f.write(str(i)+'\n')            
+            f.write(str(i)+'\n')        
+            
+    if irisOut is not None:
+        with open('iris_class_test.txt','w') as f:
+            for i in allIds[:nTest]:
+                f.write(irisOut[i]+'\n')
     
 def checkAccuracy (real, predict):
+    with open(real,'r') as f:
+        real = f.readlines()
+        
+    with open(predict,'r') as f:
+        predict = f.readlines()
+ 
+    print real
+    print predict
+    
+    
     nReal = len(real)
     nPredict = len(predict)
     
@@ -100,7 +100,13 @@ def checkAccuracy (real, predict):
             acc += 1
     
     # Priting the final accuracy
-    print (acc/nReal)*100
+    print 'Correct predictions: ', acc, ' of ', nReal
+
+    
+def aLineCmd (k=3):
+    cmd = 'aLine --classifier --algorithm knn --features iris_data.mtx --train iris_train.txt --test iris_test.txt --labels iris_class.txt -k '+str(k)+' -o iris_class_predict.txt'
+    os.system(cmd)
+
 
 
 path = os.getcwd() # my actual path
@@ -108,6 +114,9 @@ pathIris = path + '/iris'
 
 checkingDataset(pathIris)
 irisIn, irisOut = getIris(pathIris)
-generateMtx(irisIn)
 
-generateIdsTrainTest (len(irisIn))
+generateIdsTrainTest (len(irisIn), irisOut=irisOut)
+aLineCmd(7)
+checkAccuracy ('iris_class_test.txt', 'iris_class_predict.txt')
+
+
