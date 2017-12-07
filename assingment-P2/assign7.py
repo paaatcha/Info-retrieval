399ed194857cbca00fbdb78cae997a6e0652f2c5
paaatcha <pacheco.comp@gmail.com>
2017-12-07 17:55:04 -0200

    Adding P2 and updating some others

--- assingment-P2/assign7.py
+++ assingment-P2/assign7.py
@@ -7,6 +7,7 @@ Email: pacheco.comp@gmail.com
 
 """
 import os
+import sys
 import glob
 from collections import Counter
 import numpy as np
@@ -14,8 +15,7 @@ from random import shuffle
 from docx import Document
 from nltk import ngrams
 import json
-import re
-
+from metrics import *
 
 def generateFilesPath (path,shuf=False,fileFormat='txt'):
     allFolders = glob.glob(path+'/*')
@@ -56,7 +56,7 @@ def checkingDataset (path):
         os.system('wget http://www.inf.ufes.br/~elias/dataSets/usielCarneiro.tar.gz')    
         os.system('mkdir usielCarneiro')
         print 'Extracting the usielCarneiro database...'
-        os.system('tar -xzf usielCarneiro.tar.gz -C '+ path)        
+        os.system('tar -xzf usielCarneiro.tar.gz -C '+ pathFiles)        
         os.system('rm -rf usielCarneiro.tar.gz')
         print 'The files have been set'
     else:
@@ -142,38 +142,39 @@ def getSimilarityALine ():
     cmd = 'aLine --similarity -d aLineUsielSimi --features aLineUsiel/cache.txt'
     os.system(cmd)
 
-def getIndexingALine ():
-    cmd = 'aLine -i -l filesPathUsiel.txt -d aLineUsiel'
-    os.system(cmd)
+#def getIndexingALine ():
+#    cmd = 'aLine -i -l filesPathUsiel.txt -d aLineUsiel'
+#    os.system(cmd)
 
 
-def getSpaceDensity (filesList, amount=5):    
-    sMat = np.loadtxt('similaridade.mtx', skiprows=2)
-    simi = sum(sMat[:,2])
-    print '\n**** Space density: ', simi-176, ' ****'
-    print '**** Average of similarity between the docs pairwise: ', simi/len(sMat), ' *****'
-    
-    
-    sMat = sMat[sMat[:,2].argsort()]
-    n = sMat.shape[0]
-    ns = 0
-    mostSimi = list()
-    included = list()
-    for k in xrange(n-1,0,-1):
-        t = (sMat[k][0], sMat[k][1])
-        if (t[0] != t[1]) and ((t[1],t[0]) not in included):
-            ns += 1
-            included.append(t)
-            mostSimi.append(sMat[k][:])
-            if ns == amount:
-                break
-
-    print '\n*** The most similarities files***'
-    for k in mostSimi:
-        print '--- Similarity: ', k[2], ' ---'
-        print 'File 1: ', filesList[int(k[0])][59:]
-        print 'File 2: ', filesList[int(k[1])][59:]
-        print '\n'
+#def getSpaceDensity (filesList, amount=5):    
+#    sMat = np.loadtxt('similaridade.mtx', skiprows=2)
+#    simi = sum(sMat[:,2])
+#    print '\n**** Space density: ', simi-176, ' ****'
+#    print '**** Average of similarity between the docs pairwise: ', simi/len(sMat), ' *****'
+#    
+#    
+#    sMat = sMat[sMat[:,2].argsort()]
+#    n = sMat.shape[0]
+#    ns = 0
+#    mostSimi = list()
+#    included = list()
+#    aLineCmdVecs (fold)
+#    for k in xrange(n-1,0,-1):
+#        t = (sMat[k][0], sMat[k][1])
+#        if (t[0] != t[1]) and ((t[1],t[0]) not in included):
+#            ns += 1
+#            included.append(t)
+#            mostSimi.append(sMat[k][:])
+#            if ns == amount:
+#                break
+#
+#    print '\n*** The most similarities files***'
+#    for k in mostSimi:
+#        print '--- Similarity: ', k[2], ' ---'
+#        print 'File 1: ', filesList[int(k[0])][59:]
+#        print 'File 2: ', filesList[int(k[1])][59:]
+#        print '\n'
 
 def getDictionaryALine (path):
     arch = open(path,'r')
@@ -232,115 +233,7 @@ def getMostCommon (indexWords, amount=30):
     print '\n**** The most common nouns ****'
     print '\n'.join('Word: %s - Freq: %s' % x for x in nouns)
         
-def isDate (words):
-    days = ('SEGUNDA', 'TERÇA', 'TERCA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'SABADO', 'DOMINGO')
-    months = ('JANEIRO', 'FEVEREIRO', 'MARÇO', 'MARCO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO')
-    prob, day, month = 0.0, None, None
-    words = words.replace('/',' ').replace(',','').replace('\\',' ').replace('\n','').split(' ')
-    siz = len(words)
-    for w in words:            
-        if w.upper() in days:
-            day = w
-            prob += 1.0
-        if w.upper() in months:            
-            month = w
-            prob += 1.0
-    
-    prob /= siz
-    return prob > 0.1, day, month
-  
-def isTitle (words):
-    #p,_,_= isDate (words)
-    return (len(words) <= 100 and len(words) > 3 )
-    
-
-def isVers (words):    
-    bibleBooks = ('gênesis', 'genesis', 'êxodo', 'exodo', 'levítico', 'levitico', 'números', 'numeros', 'deuteronômio',\
-    'deuteronomio', 'josué', 'josue', 'juizes', 'juízes', 'rute', 'samuel', 'reis', 'cronicas', 'crônicas', 'esdras',\
-    'neemias', 'ester', 'jo', 'jó',  'salmos', 'proverbios', 'provérbios', 'eclesiastes', 'cantico', 'cântico',\
-    'salomao', 'salomão', 'isaias', 'isaías',  'jeremias', 'lamentacoes', 'lamentações', 'ezequiel', 'daniel',\
-    'oseias', 'joel', 'amós', 'amos', 'obadias', 'jonas', 'miqueias', 'naum', 'habacuque', 'sofonias', 'ageu',\
-    'zacarias', 'malaquias', 'mateus', 'marcos', 'lucas', 'joão', 'joao', 'romanos', 'coríntios', 'corintios',\
-    'gálatas', 'galatas', 'efésios', 'efesios',  'filipenses', 'colossenses', 'tessalonicenses', 'timóteo',\
-    'tito', 'filêmon', 'filemon', 'hebreus', 'tiago', 'pedro', 'judas', 'apocalipse')
-    
-    abrev = ('1Co','1Cr','1Jo','1Pe','1Rs','1Sm','1Tm','1Ts','2Co','2Cr','2Jo','2Pe','2Rs','2Sm','2Tm','2Ts',\
-    '3Jo','Ag','Am','Ap','At','Cl','Ct','Dn','Dt','Ec','Ed','Ef','Ex','Ez','Fm','Fp','Gl','Gn','Hb','Hc','Is',\
-    'Jd','Jl','Jn','Jo','Jó','Jr','Js','Jz','Lc','Lm','Lv','Mc','Ml','Mq','Mt','Na','Ne','Nm','Ob','Os','Pv',\
-    'Rm','Rt','Sf','Sl','Tg','Tt','Zc')    
-    
-    okSize = len(words) < 300    
-    vers = re.findall('\([^)]+\)',words)
-    
-    okBook = False
-    for v in vers:
-        book = v.replace('(','').replace(')','').split(' ')[0].lower()
-        if book in bibleBooks or book in abrev:
-            okBook = True
         
-    return okBook and okSize
-    
-
-
-def isText (words):
-    pass
-        
-def splitPatterns (path, pathNew, verbose=False):  
-    
-    date, title = None, None
-    
-    
-    with open(path,'r') as f:
-        lines = f.readlines()
-        
-    k = 0
-    while(k < len(lines)):
-        newText = ''
-        ok = False
-        # Checking the date
-        p, d, m = isDate(lines[k])
-        if p:       
-            newText += '<DATA_DO_DOCUMENTO>\n' + lines[k]
-            
-            # Go forward and find the title
-            k += 1
-            p = isTitle (lines[k])
-            if p:
-                newText += '\n<TITULO_DO_DOCUMENTO>\n' + lines[k]
-                title = lines[k]                
-                
-                # Go forward and find the bible citation
-                k += 1
-                p = isVers (lines[k])
-                if p:
-                    newText += '\n<CITACAO_DA_BIBLIA>\n' + lines[k]
-                
-                    # Go forward and find the bible citation
-                    k += 1
-                    newText += '\n<TEXTO_DO_PASTOR>\n' + lines[k]
-                    ok = True
-            
-        if ok:
-            if m is not None:                
-                pathFolder = pathNew + '/' + m            
-                pathName = pathFolder + '/' + title[:-1] + '.txt'            
-                if verbose:                                
-                    print newText           
-                    print pathNew
-                    print pathName
-                    
-                isDir = os.path.isdir(pathFolder)
-                if not isDir:
-                    os.mkdir(pathFolder)         
-                
-                with open(pathName, 'w') as f:
-                    f.write(newText)
-        # Getting a new line
-        k += 1
-            
-    
-
-
 
 
 #################### STARTING THE SCRIPT ###########################
@@ -348,14 +241,14 @@ def splitPatterns (path, pathNew, verbose=False):
 path = os.getcwd() # my actual path
 pathFiles = path + '/usielCarneiro'  
 
-# Cheking the database and download it if needsOUTBRO
+# Cheking the database and download it if needs
 checkingDataset (pathFiles)
 
 # Getting the docx paths
 filesListDocx = generateFilesPath (pathFiles, fileFormat='docx')
 
 # Converting the docx to txt
-#docx2txt(filesListDocx)
+docx2txt(filesListDocx)
 
 # Getting the txt paths
 filesListTxt = generateFilesPath (pathFiles, fileFormat='txt')
@@ -367,7 +260,33 @@ pathFiles = writeNFilesPath (filesListTxt,n,'Usiel')
 # Getting the n-grams
 #getNGrams (pathFiles, 30)
 
-for files in filesListTxt:
-    splitPatterns (files, path, False)
+
+##############################  aLINE  ####################################
+#getIndexingALine()
+#getSimilarityALine()  
+
+
+fold = 'usiel/'
+#fil = filesListTxt
+
+#s = int(sys.argv[1])
+k = int(sys.argv[1])
+fold = fold
+#getNFilesPath (filesList,s)
+fil = 'filesPathUsiel.txt'
+
+aLineCmdIndex(fil, fold)
+aLineCmdCluster (fold, k, nIter=100)
+aLineCmdVecs (fold)
+
+dimVec = getDimVec (fold+'dataset.conf')
+#dimVec = getDimVec ('dataset.conf')
+vecs = getVectors (fold+'features.mtx', dimVec, 176)
+#vecs = getVectors ('features.mtx', dimVec, s)
+dataClusters = clustersInDict (vecs, 'output.clustering')
+centroids = getCentroids('centroids.mtx',k, dimVec) 
+
+
+getMetricsTable (vecs, centroids, dataClusters, k)
 
 
