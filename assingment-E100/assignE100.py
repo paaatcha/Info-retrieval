9d5af3cca68c4322f85be0110fffa8438504afc8
paaatcha <pacheco.comp@gmail.com>
2017-12-17 21:20:25 -0200

    update E100

--- assingment-E100/assignE100.py
+++ assingment-E100/assignE100.py
@@ -18,6 +18,7 @@ import subprocess
 import urllib
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
+from itertools import combinations
 
 sys.path.append ('Google')
 from google2 import search
@@ -30,15 +31,21 @@ def formatText (text):
     return text
 
 def loadQuery (path):
+    allQueries = list()
     with open(path,'r') as f:
-        q = formatText(f.read())
-    q = removeStopWords(q)        
-    q = q.split()
-    print len(q)
+        lines = f.readlines()
+    for q in lines:    
+        if len(q) > 5:
+            allQueries.append(removeStopWords(q).split())
+        
+    return allQueries
+        
+    
     
     return q
 
 def removeStopWords (text):
+    text = formatText (text)
     with open('stop-words','r') as f:
         stopWords = f.read().replace(' ','').decode('utf-8').split('\n')
         
@@ -51,53 +58,100 @@ def removeStopWords (text):
     result = ' '.join(newText)
     return result
 
+def isHTML (url):
+    typ = url.split('/')[-1].split('.')[-1].lower()
+    if typ == 'pdf' or typ == 'doc' or typ == 'docx':
+        return False
+    else:
+        return True
 
 def getURLsFromQuery (query):
-    sg = search(query, tld='com', lang='pt-br', stop=5)
+    sg = search(query, tld='com', lang='pt-br', num=5, stop=2, only_standard=True)
     #sg = ['http://www.pachecoandre.com.br']
     
     allTexts = list()   
     allURLs = list()
     for url in sg:    
-        print url, '\n'
-        allURLs.append(url)
-        html = urllib.urlopen(url).read()
-        soup = BeautifulSoup(html)
-    
-        # killing all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()
-    
-        # getting the text
-        text = soup.get_text()  
+        print 'URLs encontrada: \n', url, '\n'
+        if isHTML (url):
+            allURLs.append(url)
+            html = urllib.urlopen(url).read()
+            soup = BeautifulSoup(html, "html")
         
-        # Formating the text
-        lines = (line.strip() for line in text.splitlines())
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-        allTexts.append(text)
-        text = ''
+            # killing all script and style elements
+            for script in soup(["script", "style"]):
+                script.extract()
+        
+            # getting the text
+            text = soup.get_text()  
+            
+            # Formating the text
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+            allTexts.append(text)
+            text = ''
 
     return allTexts, allURLs
 
- 
+def compare (wordsQuery, fullText, URL, sizeCompT=100, sizeCompQ=100, threshold=0.3):
+    wordsText = removeStopWords(fullText).split(' ')
+    sizeText = len(wordsText)     
 
+    sizeQuery = len(wordsQuery)
+
+    if sizeCompT > sizeText:
+        sizeCompT = sizeText
+    if sizeCompQ > sizeQuery:
+        sizeCompQ = sizeQuery
+    
+    output = ''
+    for offset_q in range(0,sizeQuery,sizeCompQ):
+        end_q = offset_q + sizeCompQ
+        compQuery = ' '.join(wordsQuery[offset_q:end_q])         
+
+        for offset in range(0,sizeText,sizeCompT):
+            end = offset + sizeCompT
+            compText = ' '.join(wordsText[offset:end])    
+            ratio = SequenceMatcher(None,compQuery,compText).ratio()
+            
+            if ratio > threshold:                
+                output += '## SUSPEITA DE PLÁGIO ENCONTRADA ##\n'
+                output += 'URL: ' + URL + '\n\n'
+                output += '## PARTE DO TEXTO SUSPEITA ##\n'
+                output += compQuery+'\n\n'                    
+                
+                
+                
+    return output
+                
+            
+
+        
+        
 
 #################### STARTING THE SCRIPT ###########################
 pathQuery = 'query.txt'
-query = loadQuery (pathQuery)
+fullQuery= loadQuery (pathQuery)
 
-print query
-#allTexts, allURLs = getURLsFromQuery(query)
+output = ''
+b=1
+nw = 15
+for query in fullQuery: 
+    if nw > len(query):
+        nw = len(query)
+    smallQuery = ' '.join(query[0:nw])
+    print 'Busca {}...\n'.format(b)
+    allTexts, allURLs = getURLsFromQuery(smallQuery)
+    b+=1
+    
+    for k in range(len(allTexts)):
+        output += compare (query, allTexts[k], allURLs[k])
+    
+with open('Resultado.txt', 'w') as f:
+    f.write(output)
     
 
-#similarity_ratio = SequenceMatcher(None,removeStopWords(comp), removeStopWords(text)).ratio()
-#print similarity_ratio  #plagiarism detected   
-
-
-
-
-#removeStopWords (text)
 
 
 
